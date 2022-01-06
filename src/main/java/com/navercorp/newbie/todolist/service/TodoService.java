dd8265ca67f38ac0152030a09e9a22bac993e91c
Taejoong Kim <tae-joong.kim@navercorp.com>
2022-01-06 11:57:46 +0900

    meaningless commit

--- src/main/java/com/navercorp/newbie/todolist/service/TodoService.java
+++ src/main/java/com/navercorp/newbie/todolist/service/TodoService.java
@@ -34,8 +34,6 @@ public class TodoService {
         return false;
     }
 
-    // log4j, logback 로컬에 파일, 실시간 전송
-
     public Todo createTodo(TodoCreateForm todoCreateForm) throws Exception {
         Todo todo = new Todo();
         todo.create(todoCreateForm);
