292b6ed042d62bd23820fdf2405f90f5a69349df
Taejoong Kim <tae-joong.kim@navercorp.com>
2022-01-06 12:12:01 +0900

    remove testcode

---
+++ src/test/java/com/navercorp/newbie/todolist/controller/FileControllerTest.java
@@ -0,0 +1,12 @@
+package com.navercorp.newbie.todolist.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileControllerTest {
+
+    @Test
+    void getFileResource() {
+    }
+}
